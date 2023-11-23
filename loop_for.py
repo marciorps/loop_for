b4df7376500530befbc63865a4c81b8ece9604cc
marciorps <marciorps@yahoo.com,.br>
2023-11-23 16:32:21 -0300

    loop_for

---
+++ loop_for.py
@@ -0,0 +1,17 @@
+for numero in range(1,21, 2):
+    print('carregando...', numero)
+
+nomes = ['marcio', 'greice', 'aurora', 'Lindinha', 'paçoca']
+for nome in nomes:
+    print(nome)
+
+    #Desafio
+    ##Usando um loop, exiba na tela: Estamos em X onde x é um valor iniciando em 18 e finalizando em 110
+
+for numero in range(18, 111):
+    print("estamos em", numero)
+
+    #desafio2
+    ## Você precisa de 10 passos para finalizar uma tarefa, exiba na tela, usando loop for a seguinte frase "Realizando passo X"
+for passo in range(1, 11):
+    print(f"Realizando passo {passo}")
